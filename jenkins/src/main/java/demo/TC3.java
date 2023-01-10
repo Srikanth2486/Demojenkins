7d3c97cf874d5a593fa880bcd0bc1ca897daf695
PC <PC@DESKTOP-211RB46>
2023-01-10 15:53:30 +0530

    2nd commit

---
+++ jenkins/src/main/java/demo/TC3.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC3 {
+	@Test
+	public void m3(){
+		Reporter.log("haiii",true);
+		
+	}
+
+}
